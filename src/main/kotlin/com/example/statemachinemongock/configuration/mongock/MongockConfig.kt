f76f2196a939b8541d4435e5cca285255953af89
Marcio Dalfre <marciodalfre@gmail.com>
2021-08-02 18:05:35 -0300

    Mongock configuration:
    - Mongock base configuration created;
    - Created changelog to test.

---
+++ src/main/kotlin/com/example/statemachinemongock/configuration/mongock/MongockConfig.kt
@@ -0,0 +1,41 @@
+package com.example.statemachinemongock.configuration.mongock
+
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongoV3Driver
+import com.github.cloudyrock.spring.v5.MongockSpring5
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.MongoTemplate
+
+@Configuration
+class MongockConfig {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    val changelogPackage = "com.example.statemachinemongock.configuration.mongock.changelog"
+    private val allowCollectionCreation = true
+    private val changelogCollectionName = "statemachineChangelog"
+
+    @Bean
+    fun mongockBuilder(
+        mongoTemplate: MongoTemplate,
+        applicationContext: ApplicationContext
+    ): MongockSpring5.MongockApplicationRunner {
+
+        logger.info("Starting Mongock ...")
+
+        val driver = SpringDataMongoV3Driver
+            .withDefaultLock(mongoTemplate)
+
+        driver.changeLogRepositoryName = changelogCollectionName
+        driver.isIndexCreation = allowCollectionCreation
+
+        return MongockSpring5.builder()
+            .setDriver(driver)
+            .addChangeLogsScanPackage(changelogPackage)
+            .setSpringContext(applicationContext)
+            .buildApplicationRunner()
+    }
+}

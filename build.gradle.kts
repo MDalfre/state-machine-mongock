374df9ea6dd0c812e657f105f5fa703e632bf6a5
Marcio Dalfre <marciodalfre@gmail.com>
2021-08-02 15:56:16 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.5.3"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.5.21"
+	kotlin("plugin.spring") version "1.5.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+val mongockVersion = "4.3.8"
+val stateMachineVersion = "2.1.3.RELEASE"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.springframework.statemachine:spring-statemachine-core:$stateMachineVersion")
+
+	implementation("com.github.cloudyrock.mongock:mongock-bom:$mongockVersion")
+	implementation("com.github.cloudyrock.mongock:mongock-spring-v5:$mongockVersion")
+	implementation("com.github.cloudyrock.mongock:mongodb-springdata-v3-driver:$mongockVersion")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/example/statemachinemongock/StateMachineMongockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.statemachinemongock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StateMachineMongockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

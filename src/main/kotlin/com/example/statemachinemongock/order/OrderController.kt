90c05f3e677bfd3758a9949ec9ef5b4cbf44a1a0
Vitor <vitorghis@gmail.com>
2021-08-06 16:25:00 -0300

    fix controller annotation

--- src/main/kotlin/com/example/statemachinemongock/order/OrderController.kt
+++ src/main/kotlin/com/example/statemachinemongock/order/OrderController.kt
@@ -1,10 +1,10 @@
 package com.example.statemachinemongock.order
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController()
 @RequestMapping("/order")
 class OrderController(
     private val orderService: OrderService
